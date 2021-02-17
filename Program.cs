75558cf65fd669dd0937f3c1502ffd85d01e1dc9
almag <grimberglma@gmail.com>
2021-02-17 20:00:04 -0300

    Inicializacion de la aplicacion con la primer impresion

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mi_primer_calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi primer calculadora ♥");
+        }
+    }
+}
